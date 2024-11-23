54fb027af00983d0bd8400929afb88d8f0288a7c
fermarsan <fmartinezsanta@gmail.com>
2024-11-22 22:16:32 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# arduino-cli instaling script
+
+This installs _arduino-cli_ v1.1.1 for Windows x64.
+
+Follow these steps:
+
+- open _PowerShell_ as administrator 
+- run the command: `Set-ExecutionPolicy RemoteSigned`
+- from _PowerShell_ open the folder with the script `arduino-cli_install.ps1`
+- run the script as: `.\arduino-cli_install.ps1`
---
+++ arduino-cli_install.ps1
@@ -0,0 +1,22 @@
+Set-PSDebug -Trace 0    #echo off
+
+# this is a batch file to automate the installing of arduino-cli on Windows
+# copy 'arduino-cli.zip' in the same folder of this file after executing as administrator
+
+# Unzip arduino-cli.zip to C:\arduino-cli
+if (-not (Test-Path -Path 'C:\arduino-cli')) {    # if the folder doesn't exist
+    Write-Output 'Unzipping arduino-cli.zip to C:\arduino-cli...'
+    Expand-Archive -Path '.\arduino-cli_1.1.1_Windows_64bit.zip' -DestinationPath 'C:\arduino-cli'
+} 
+
+# Create arduino-cli symlink
+if (-not (Get-Command arduino-cli)) { # if the command doesn't exist
+    Write-Output "Creating arduino-cli symlink..."
+    $env:PATH += ";C:\arduino-cli\"
+    [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
+}
+
+arduino-cli core install arduino:avr
+
+Write-Output "Process completed."
+Read-Host -Prompt "Press any key to continue"
