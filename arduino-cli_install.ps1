89f40b74a3b8d63b90b082b246719d2974f2e5ec
fermarsan <fmartinezsanta@gmail.com>
2024-11-30 18:09:59 -0500

    check if the executable exists

--- arduino-cli_install.ps1
+++ arduino-cli_install.ps1
@@ -2,20 +2,20 @@ Set-PSDebug -Trace 0    #echo off
 
 # this is a batch file to automate the installing of arduino-cli on Windows
 
-# Download arduino-cli.zip from the official page
-if (-not (Test-Path -Path "..\arduino-cli.zip")) {  # if the .zip file doesn't exist
-    Write-Output "`nDownloading arduino-cli.zip from the official page..."
-    Invoke-WebRequest "https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip" -OutFile "..\arduino-cli.zip"
-}
-
-# Unzip arduino-cli.zip to C:\arduino-cli
-if (-not (Test-Path -Path "C:\arduino-cli")) {    # if the folder doesn't exist
-    Write-Output "`nUnzipping arduino-cli.zip to C:\arduino-cli..."
-    Expand-Archive -Path "..\arduino-cli.zip" -DestinationPath "C:\arduino-cli"
-} 
-
-# Create arduino-cli symlink (add it to PATH)
 if (-not (Get-Command arduino-cli)) { # if the command doesn't exist
+    # Download arduino-cli.zip from the official page
+    if (-not (Test-Path -Path "..\arduino-cli.zip")) {  # if the .zip file doesn't exist
+        Write-Output "`nDownloading arduino-cli.zip from the official page..."
+        Invoke-WebRequest "https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip" -OutFile "..\arduino-cli.zip"
+    }
+
+    # Unzip arduino-cli.zip to C:\arduino-cli
+    if (-not (Test-Path -Path "C:\arduino-cli")) {    # if the folder doesn't exist
+        Write-Output "`nUnzipping arduino-cli.zip to C:\arduino-cli..."
+        Expand-Archive -Path "..\arduino-cli.zip" -DestinationPath "C:\arduino-cli"
+    } 
+
+    # Create arduino-cli symlink (add it to PATH)
     Write-Output "`nCreating arduino-cli symlink..."
     $env:PATH += ";C:\arduino-cli\"
     [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
